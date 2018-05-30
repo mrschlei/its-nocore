a2dcf6df9a8ef7412b7c06ba376fdae886cec6da
mrschlei <mrschlei@umich.edu>
2018-05-30 16:12:55 -0400

    bad ideas

--- start.sh
+++ start.sh
@@ -18,6 +18,10 @@ ln -sf /secrets/ssl/USERTrustRSACertificationAuthority.pem /etc/ssl/certs/USERTr
 ln -sf /secrets/ssl/AddTrustExternalCARoot.pem /etc/ssl/certs/AddTrustExternalCARoot.pem
 ln -sf /secrets/ssl/sha384-Intermediate-cert.pem /etc/ssl/certs/sha384-Intermediate-cert.pem
 
+#bad ideas
+export ORACLE_HOME=/usr/lib/oracle/12.2/client64
+export LD_LIBRARY_PATH=/opt/oracle/instantclient
+
 if [ -f /secrets/app/local.start.sh ]
 then
   /bin/sh /secrets/app/local.start.sh
